3de3e5c41d2aa607894431fcfd9728775c740c04
Hannah Melendy <hrmelendy@gmail.com>
2019-06-08 10:17:34 -0700

    add five questions to html; collect responses in js/scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  var teaTimeInput = parseInt($(".teaTime").val());
+  var valuableInput = parseInt($(".valuable").val());
+  var weekendInput = parseInt($(".weekend").val());
+  var garbageInput = parseInt($(".garbage").val());
+  var arguementInput = parseInt($(".arguement").val());
+  console.log(arguementInput);
+
+
+});
